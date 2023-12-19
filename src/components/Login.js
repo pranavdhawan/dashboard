7e516775e24fa5ba72413cbb3ccc849a1f38c861
Pranav Dhawan <dhawan.pranav8@gmail.com>
2023-12-19 14:21:07 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,50 +1,61 @@
-// login.js
+import { useState } from 'react'
 
-import { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext'
 
-function Login() {
-  const { login } = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function App() {
+    const {setUser} = useAuth()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 
-  const loginUser = async (event) => {
-    event.preventDefault();
 
-    try {
-      // Call the login function from the useAuth hook
-      await login(email, password);
-      alert('Login successful');
-      // Redirect or perform other actions after successful login
-      window.location.href = '/';
-    } catch (error) {
-      console.error('Login failed:', error.message);
-      alert('Please check your username and password');
-    }
-  };
+	async function loginUser(event) {
+		event.preventDefault()
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={loginUser}>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Email"
-        />
-        <br />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
+		const response = await fetch('http://localhost:1337/api/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+
+		if (data.user) {
+			localStorage.setItem('token', data.user)
+			alert('Login successful')
+            setUser(email)
+			window.location.href = '/'
+		} else {
+			alert('Please check your username and password')
+		}
+	}
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<form onSubmit={loginUser}>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email"
+				/>
+				<br />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input type="submit" value="Login" />
+			</form>
+		</div>
+	)
 }
 
-export default Login;
+export default App

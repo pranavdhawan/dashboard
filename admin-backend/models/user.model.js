223a3f4de0ac7fa1c0ebdd2be7536042c826bdac
Pranav Dhawan <dhawan.pranav8@gmail.com>
2024-11-29 00:25:35 +0530

    Added indexing to user model

--- admin-backend/models/user.model.js
+++ admin-backend/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const User = new mongoose.Schema(
 	{
 		// name: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true, index: true},
 		password: { type: String, required: true },
 		sheetId: { type: String },
 	},
